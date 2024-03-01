8ac8dbc85954b5f3a9ba97118f6e3a5a8884c463
EardwulfC <eardwulfcomfy@gmail.com>
2024-03-01 17:54:46 -0500

    More WIP changes

--- YolkMe/Core/EggGrowPatch.cs
+++ YolkMe/Core/EggGrowPatch.cs
@@ -7,11 +7,9 @@ namespace YolkMe
     {
         [HarmonyPostfix]
         [HarmonyPatch(nameof(EggGrow.GrowUpdate))]
-        public static void GrowUpdatePostfix(EggGrow __instance)
-        {
+        public static void GrowUpdatePostfix(EggGrow __instance) {
 
-            if (__instance.m_nview && __instance.m_nview.IsValid())
-            {
+            if (__instance.m_nview && __instance.m_nview.IsValid()) {
 
                 float growStart = __instance.m_nview.m_zdo.GetFloat(ZDOVars.s_growStart, 0f);
                 // growStart is reset to 0 during GrowUpdate() if CanGrow() returns false.
